b7a87b016477710014e8d4b2d667d6a1e9dc3822
Ragav Kumar <27591832+rkain101@users.noreply.github.com>
2019-07-04 11:00:41 -0700

    Clean up of less file, introducing colour variables Currently in process of implemeneting a consist naming scheme for cell classes.

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
 $(function () {
 	$("#cell-news").tabs();
 });
-
-document.getElementById('tab1').onclick = changecolor(1);
-document.getElementById('tab2').onclick = changecolor(2);
-document.getElementById('tab3').onclick = changecolor(3);
-function changecolor(n){
-	if(n == 1){
-		document.getElementById('tab1').style.backgroundColor = "red";
-	}
-	else if(n == 2){
-		document.getElementById('tab2').style.backgroundColor = "red";
-	}
-	else{
-		document.getElementById('tab3').style.backgroundColor = "red";
-	}
-	return false;
-}
