4b1c5451c96491c8b9044f724a9eb043d5e86cdf
Ragav Kumar <27591832+rkain101@users.noreply.github.com>
2019-07-04 12:36:50 -0700

    implemented starring behaviour

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(function () {
+	// ----- jQuery starts here -----
+	// News tabs (via jquery UI)
 	$("#cell-news").tabs();
+	// 
+	// Bus favourites
+	$("#cell-bus i").click(function () { 
+		// Toggle star behaviour
+		$(this).toggleClass("far").toggleClass("fas");
+		if ($(this).hasClass("fas")) {
+			$(this).parent().addClass("star");
+		} else {
+			$(this).parent().removeClass("star");
+		}
+	});
+	// ----- jQuery ends here -----
 });
