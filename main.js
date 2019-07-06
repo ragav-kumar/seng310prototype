7f8e06d3dbb9db375490970d30752af094b3a8c0
Ragav Kumar <27591832+rkain101@users.noreply.github.com>
2019-07-05 17:39:33 -0700

    syncing changes between devices

--- main.js
+++ main.js
@@ -13,5 +13,18 @@ $(function () {
 			$(this).parent().removeClass("star");
 		}
 	});
+	// Header Icons
+	$(".email-icon").click(function (e) { 
+		e.preventDefault();
+		alert("");
+	});
+	function headerIconClick(e) {
+		e.preventDefault();
+		if ($(this).hasClass("email-icon")) {
+			alert("This would open uvic webmail in a simulated browser view.");
+		} else if ($(this).hasClass("notification-icon")) {
+			window
+		}
+	}
 	// ----- jQuery ends here -----
 });
