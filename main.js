e711337b680020ba7b522be31d96ef6b4145a907
Ragav Kumar <27591832+rkain101@users.noreply.github.com>
2019-07-08 14:25:11 -0700

    implemented login screen

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(function () {
+	// used to identify which button was hit in a form
+	let submitAction;
 	// ----- jQuery starts here -----
+	// Allows closing popup by clicking black area, without breaking children clicks.
+	$(".popup-wrap").click(function (e) {
+		if (e.target !== this) return;
+		e.preventDefault();
+		$(".popup-wrap, .popup-wrap popup").hide();
+	});
 	// News tabs (via jquery UI)
 	$("#cell-news").tabs();
 	$(".flipswitch").flipswitch({
@@ -8,7 +16,7 @@ $(function () {
 		width: 50,
 		init: "right"
 	});
-	// 
+	
 	// Bus favourites
 	$("#cell-bus i").click(function () { 
 		// Toggle star behaviour
@@ -37,6 +45,28 @@ $(function () {
 		e.preventDefault();
 		$(".popup-wrap").hide();
 		$(".popup-settings").hide();
+	});
+	$(".user-icon.logged-out").click(function(e) {
+		e.preventDefault();
+		$(".popup-wrap").show();
+		$(".popup-login").show();
+	});
+	// Store name of submit button, allowing 2 submit buttons
+	$("input[type=submit]").click(function() {
+		submitAction = $(this).attr("name");
+	});
+	$("#form-login").submit(function(e) {
+		e.preventDefault();
+		if (submitAction == "cancel") { // close login popup
+			$(".popup-wrap").hide();
+			$(".popup-login").hide();
+		} else {
+			if ($("#login-id").val() && $("#login-password").val()) {
+				window.location.href = "loggedin.html";
+			} else { // error message
+				alert("Please enter your netlink ID and password");
+			}
+		}
 	});
 	// ----- jQuery ends here -----
 });
